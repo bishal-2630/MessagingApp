036c03ea14c17b3ae57b34cadf9d729000b793cf
Bishal <kbishal177@gmail.com>
2026-06-21 20:44:09 +0545

    feat: configure dynamic PostgreSQL connection with Neon integration using dj-database-url

--- core/settings.py
+++ core/settings.py
@@ -63,12 +63,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
+
+if not DATABASES['default']:
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
 
 
 # Password validation
