99a3cbe8d988d31ffa2218c00aa9763087293ee0
Bishal <kbishal177@gmail.com>
2026-07-14 17:48:06 +0545

    Created register and login APIs

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from . import views 
+from .views import RegisterView, LoginView
 
 urlpatterns = [
-    path('health/', views.health_check, name='health_check'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,28 @@
+from rest_framework import status
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 
-def health_check(request):
-    return JsonResponse({'status':'ok', 'message': 'Backend is running'})
+class RegisterView(APIView):
+    permission_classes = []
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    permission_classes = []
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key,'user_id': user.id, 'username': user.username})
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
